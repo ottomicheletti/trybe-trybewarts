ac8ccfe0f1ec7d5195463c289b41c036c0842a09
Otto Micheletti <michelettiotto@gmail.com>
2022-01-11 16:42:12 -0300

    Requisito 5, 6 e 7

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Requisito 3
-const btnLogin = document.querySelector("#btn-login");
-const email = document.querySelector("#email");
-const senha = document.querySelector("#senha");
+const btnLogin = document.querySelector('#btn-login');
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
 
-btnLogin.addEventListener("click", function () {
-  if (email.value === "tryber@teste.com" && senha.value === "123456") {
-    alert("Olá, Tryber!");
+btnLogin.addEventListener('click', () => {
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 });
+
