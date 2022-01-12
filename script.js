0798527be0171e0c72f08632d1ad6b9e60a21a15
Otto Micheletti <michelettiotto@gmail.com>
2022-01-12 15:59:06 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -42,3 +42,44 @@ function caracteres(event) {
 }
 
 text.addEventListener('keyup', caracteres);
+
+// Requisito 21
+const form = document.querySelector('#evaluation-form');
+const nome = document.querySelector('#input-name');
+const sobrenome = document.querySelector('#input-lastname');
+const emailForm = document.querySelector('#input-email');
+const casa = document.querySelector('#house');
+const familia = document.querySelectorAll('.family');
+let familiaEscolhida = '';
+const materias = document.querySelectorAll('.subject');
+let materiasEscolhidas = '';
+const notas = document.querySelectorAll('.nota');
+let notaEscolhida = '';
+const obervacoes = document.querySelector('textarea');
+
+// functions family(), checked() e satisfaction() inspiradas em https://stackoverflow.com/a/45875948
+function family() {
+  return [...familia].filter((radio) => radio.checked).map((radio) => radio.value);
+}
+
+function checked() {
+  return [...materias].filter((checkbox) => checkbox.checked).map((checkbox) => checkbox.value);
+}
+
+function satisfaction() {
+  return [...notas].filter((radio) => radio.checked).map((radio) => radio.value);
+}
+
+btnSubmit.addEventListener('click', () => {
+  familiaEscolhida = family();
+  materiasEscolhidas = checked();
+  notaEscolhida = satisfaction();
+  form.innerHTML = `
+  <p> Nome: ${nome.value} ${sobrenome.value} </p>
+  <p> Email: ${emailForm.value}</p>
+  <p> Casa: ${casa.value}</p>
+  <p> Família: ${familiaEscolhida}</p>
+  <p> Matérias: ${materiasEscolhidas.join(', ')}</p>
+  <p> Avaliação: ${notaEscolhida}</p>
+  <p> Observações: ${obervacoes.value}`;
+});
